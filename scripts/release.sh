a63a9acb3a2891a2e9b73fa22f0676cf3519ad87
Linh <linh.buitung`18@gmail.com>
2025-10-08 22:00:11 +0200

    test

---
+++ scripts/release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Write GCP credentials to file
+echo "$GCP_SA_KEY" > gcloud-key.json
+
+# Configure DVC to use the credentials
+dvc remote modify --local myremote credentialpath gcloud-key.json
+
+# Pull data from remote
+dvc pull -v || { echo "DVC pull failed"; exit 1; }
+
+# Optional cleanup
+rm gcloud-key.json
