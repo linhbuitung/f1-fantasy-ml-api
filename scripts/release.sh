48e4af25bb58e49000aff119ce021acad8c57c76
Linh <linh.buitung`18@gmail.com>
2025-10-08 22:55:16 +0200

    test python run

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
-# Write GCP credentials to file
-echo "$GCP_SA_KEY" > gcloud-key.json
+## Write GCP credentials to file
+#echo "$GCP_SA_KEY" > gcloud-key.json
+#
+## Tell DVC to skip Git
+#dvc config core.no_scm true
+#
+## Configure DVC to use the credentials
+#dvc remote modify --local myremote credentialpath gcloud-key.json
+#
+## Pull data from remote
+#dvc pull -v || { echo "DVC pull failed"; exit 1; }
+#
+## Optional cleanup
+#rm gcloud-key.json
 
-# Tell DVC to skip Git
-dvc config core.no_scm true
-
-# Configure DVC to use the credentials
-dvc remote modify --local myremote credentialpath gcloud-key.json
-
-# Pull data from remote
-dvc pull -v || { echo "DVC pull failed"; exit 1; }
-
-# Optional cleanup
-rm gcloud-key.json
+python scripts/rebuild_processed_data.py
+python scripts/create_mainrace_training.py
+python scripts/create_qualifying_training.py
+python scripts/create_status_training.py
+python scripts/train_mainrace_model.py
+python scripts/train_qualifying_model.py
+python scripts/train_status_model.py
